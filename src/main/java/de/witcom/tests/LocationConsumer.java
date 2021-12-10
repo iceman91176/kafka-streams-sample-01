a4680e05b3d453b2e42e63c4bb18ceeb43541269
Carsten Buchberger <c.buchberger@witcom.de>
2021-12-10 13:53:26 +0000

    still initialising

---
+++ src/main/java/de/witcom/tests/LocationConsumer.java
@@ -0,0 +1,20 @@
+package de.witcom.tests;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.jboss.logging.Logger;
+
+import de.witcom.test.schema.avro.location.GenericLocation;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class LocationConsumer {
+
+    private static final Logger LOGGER =
+        Logger.getLogger("LocationConsumer");
+    
+    @Incoming("locations-from-kafka")
+    public void receive(GenericLocation location){
+        LOGGER.infof("Received location: %s %s", location.getLocId(),location.getName());
+    }
+}
