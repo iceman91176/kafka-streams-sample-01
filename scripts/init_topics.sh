67b05a7bc4891f55af65c598ff28a2ed681782f4
Carsten Buchberger <c.buchberger@witcom.de>
2021-12-10 15:37:12 +0000

    go on

--- scripts/init_topics.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST \
-  http://schema-registry:8081/subjects/generic-location-value/versions \
-  -H 'Cache-Control: no-cache' \
-  -H 'Content-Type: application/vnd.schemaregistry.v1+json' \
-  -d '{"schema":"[ { \"type\": \"record\", \"name\": \"GenericLocation\", \"namespace\": \"de.witcom.test.schema.avro.location\", \"fields\": [ { \"name\": \"loc_id\", \"type\": \"string\" }, { \"name\": \"name\", \"type\": \"string\" } ] } ]"}'
